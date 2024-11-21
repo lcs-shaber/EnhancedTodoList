192ea7f703b1904bfb843e1f49eca841c064daa4
Sophie Haber <SHaber@lcs.on.ca>
2024-11-21 08:56:41 -0500

    Initial Commit

---
+++ EnhancedTodoList/EnhancedTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnhancedTodoListApp.swift
+//  EnhancedTodoList
+//
+//  Created by Sophie Haber on 21.11.24.
+//
+
+import SwiftUI
+
+@main
+struct EnhancedTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
