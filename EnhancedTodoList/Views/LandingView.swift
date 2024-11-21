87b62b7dc11667a44ee6f0dc73395eeec868ef09
Sophie Haber <SHaber@lcs.on.ca>
2024-11-21 09:05:26 -0500

    I organized the project.

---
+++ EnhancedTodoList/Views/LandingView.swift
@@ -0,0 +1,18 @@
+//
+//  LandingView.swift
+//  EnhancedTodoList
+//
+//  Created by Sophie Haber on 21.11.24.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandingView()
+}
